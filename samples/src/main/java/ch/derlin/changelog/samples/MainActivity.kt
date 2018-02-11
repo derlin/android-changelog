78db480934084dfce2b0699b580fc82b668f4af6
Lucy Linder <lucy.derlin@gmail.com>
2018-02-11 18:58:04 +0100

    create samples with custom theme

--- samples/src/main/java/ch/derlin/changelog/samples/MainActivity.kt
+++ samples/src/main/java/ch/derlin/changelog/samples/MainActivity.kt
@@ -1,31 +1,36 @@
 package ch.derlin.changelog.samples
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import ch.derlin.changelog.ChangelogDialog
-import ch.derlin.changelog.ChangelogDialog.createChangelogDialog
+import android.widget.AdapterView
+import ch.derlin.changelog.Changelog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val themes = listOf<Int>(R.style.AppTheme, R.style.AppTheme_ApplyCustomChangelog)
+        spinnerThemes.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(view: AdapterView<*>, v: View?, pos: Int, l: Long) {
+                setTheme(themes[pos])
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+        spinnerThemes.setSelection(0)
+
+        // set the buttons action
+        btnFullChangelog.setOnClickListener { _ -> showChangelog(0) }
+        btnCroppedChangelog.setOnClickListener { _ -> showChangelog(2) }
+        btnLatestChangelog.setOnClickListener { _ -> showChangelog(3) }
     }
 
-    fun showChangelogFull(v: View) {
-        showChangelog()
-    }
-
-    fun showChangelogV2(v: View) {
-        showChangelog(2)
-    }
-
-    fun showChangelogLatest(v: View) {
-        showChangelog(3)
-    }
-
-    private fun showChangelog(version: Int = ChangelogDialog.ALL_VERSIONS) {
-        ChangelogDialog.createChangelogDialog(this, version).show()
+    private fun showChangelog(version: Int = Changelog.ALL_VERSIONS) {
+        Changelog.createDialog(this, versionCode = version).show()
     }
 }
